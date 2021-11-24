6aff21b0038cef7089f3699ec64b8327d584233f
RoohAllahKhan <roohallahkhan20@gmail.com>
2021-11-24 13:24:19 +0500

    Grouping transitions to manage different animations on different timings.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
+import { animate, group, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -95,11 +95,16 @@ import { Component } from '@angular/core';
         ]))
       ]),
       transition('* => void', [
-        animate(300, style({
-          transform: 'translateX(100px)',
-          opacity: 0
-        }))
-      ]),
+        group([
+          animate(300, style({
+            color: 'red'
+          })),
+          animate(300, style({
+            transform: 'translateX(100px)',
+            opacity: 0
+          })),
+        ])
+      ])
       // transition('highlighted => normal', animate(800)),
     ]),
   ]
